ced660b89075b1a1c55ba375986f976948f34c83
EMRE BAYRAM <140724234+Emre23-QA@users.noreply.github.com>
2023-08-24 23:01:32 +0300

    39. ders sonu

--- src/day20_arrays/nnn.java
+++ src/day20_arrays/nnn.java
@@ -1,9 +1,18 @@
 package day20_arrays;
 
+import java.util.Arrays;
+
 public class nnn {
     public static void main(String[] args) {
      int ll =33666323;
      String hjj=""+ll;
         System.out.println(hjj);
+        String[] str = {"ali", "vv","dd"};
+        String[] str2 = {"a", "v2","d2"};
+        str=str2;
+        str2[0]="ss";
+        System.out.println(Arrays.toString(str));
+        System.out.println(Arrays.toString(str2));
+
     }
 }
